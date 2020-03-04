5bd96a403a9914cdf118bd1549a493cec458e0b8
Ya_Myn <ricky.month@gmail.com>
2020-03-04 14:32:57 +0200

    fix(statistic): created new methods agregation with dayofyear sort

--- src/components/User/services/statistic.js
+++ src/components/User/services/statistic.js
@@ -3,6 +3,24 @@ const UserModel = require('../model');
 
 /**
  * @function
+ * @param {number} dayCount - count day of year
+ * @returns {string} dateAgo
+ */
+function countMsInDays(dayCount) {
+    const msInDay = 86400000;
+    const countMS = dayCount * msInDay;
+
+    const dateLast =
+        Number(moment('2020 - 01 - 01', 'YYYY-MM-DD').format('x')) + countMS;
+    const dateAgo = moment(dateLast, 'x')
+        .utc()
+        .format('DD-MM');
+
+    return dateAgo;
+}
+
+/**
+ * @function
  * @param {number} dayCount - count day ago
  * @returns {object} userStatistic
  */
@@ -22,29 +40,29 @@ async function getUserStat(dayCount) {
         },
         {
             $project: {
-                MonthDayUTC: {
-                    $dateToString: { format: '%m-%d', date: '$createdAt' },
-                },
                 dayOfYear: 1,
                 isThisMonth: { $gte: ['$dayOfYear', lastMonthDay] },
                 count: { $add: [1] },
             },
         },
         { $match: { isThisMonth: true } },
+
         {
             $group: {
-                _id: '$MonthDayUTC',
+                _id: '$dayOfYear',
                 number: { $sum: '$count' },
             },
         },
-        { $sort: { _id: 1, number: 1 } },
+        { $sort: { _id: 1 } },
     ]);
+    console.log(userStatisticArr);
     const userStatistic = {
         labels: [],
         count: [],
     };
     userStatisticArr.map(obj => {
-        userStatistic.labels.push(obj._id);
+        const date = countMsInDays(obj._id);
+        userStatistic.labels.push(date);
         userStatistic.count.push(obj.number);
         return;
     });
